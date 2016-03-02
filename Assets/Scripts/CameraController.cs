c9c497391cf2f12113ea903c744d4b05d35061ab
Mattias Andersson <mattias8012@gmail.com>
2016-03-02 12:34:30 +0100

    started cam script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	private Vector3 leadingPos;
+	private Vector3 secondPos;
+	private Vector3 newPos;
+	private GameObject player1;
+	private GameObject player2;
+
+	// Use this for initialization
+	void Start () {
+		player1 = GameObject.FindGameObjectWithTag ("Player1");
+		player2 = GameObject.FindGameObjectWithTag ("Player2");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
