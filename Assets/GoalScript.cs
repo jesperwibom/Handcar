255b34c0ba9333c1ce98fb05f252db64478e7110
Mattias Andersson <mattias8012@gmail.com>
2016-03-08 15:42:57 +0100

    Testlevel added

---
+++ Assets/GoalScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GoalScript : MonoBehaviour {
+
+	public Text flashingText;
+	AudioSource audioSource;
+
+	void Start () {
+
+
+
+
+
+
+	}
+	void Update(){
+		
+	}
+	void OnTriggerEnter(Collider col){
+	
+		if (col.gameObject.name == "Cart") {
+			StartCoroutine (BlinkText ());
+		}
+	}
+
+	public IEnumerator BlinkText(){
+		while (true) {
+			flashingText.text = "";
+			yield return new WaitForSeconds (.5f);
+			flashingText.text = "GOOD JOB!";
+			yield return new WaitForSeconds (.5f);
+
+		}
+	}
+}
---
+++ Assets/TestLevelCameraControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestLevelCameraControl : MonoBehaviour {
+	public GameObject playerObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (playerObject.transform.position.x+2f, transform.position.y, playerObject.transform.position.z-15f);
+	}
+}
