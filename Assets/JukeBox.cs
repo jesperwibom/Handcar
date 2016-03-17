bee00a035010097b451a22ce4079124f065f2ae5
Mattias Andersson <mattias8012@gmail.com>
2016-03-17 14:25:44 +0100

    Random songs

---
+++ Assets/JukeBox.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JukeBox : MonoBehaviour {
+	public AudioSource[] songs;
+
+
+	void Start () {
+
+		Play(Random.Range (0, 6));
+
+	}
+	public void Play(int song){
+		songs [song].Play ();
+	}
+}
